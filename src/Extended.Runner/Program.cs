e75f28a717444f635c472c65d21fc23cd1b51b45
Alexandros Kritikos <akritikos@outlook.com>
2020-05-07 03:05:35 +0300

    hellooo

--- src/Extended.Runner/Program.cs
+++ src/Extended.Runner/Program.cs
@@ -5,6 +5,10 @@ namespace Kritikos.Extended.Runner
 	public sealed class Program
 	{
 		public static void Main(string[] args)
-			=> Console.WriteLine("Hello World!");
+		{
+			Console.WriteLine("Hello World!");
+			var par = string.Join(' ', args);
+			Console.WriteLine($"Arguments: {par}");
+		}
 	}
 }
--- src/Silly.Api/Startup.cs
+++ src/Silly.Api/Startup.cs
@@ -1,40 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-namespace Silly.Api
+namespace Kritikos.Silly.Api
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.AspNetCore.Http;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+	public sealed class Startup
+	{
+		public static void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+			services.AddHealthChecks();
+		}
 
-            app.UseRouting();
+		public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
-    }
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapGet("/", async context =>
+				{
+					await context.Response.WriteAsync("Hello World!");
+				});
+			});
+		}
+	}
 }
